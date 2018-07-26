a576e1f9c998152da9bdbdff6842ee34ee6e0e40
ta-tata <fujitani.takuya.fj8@is.naist.jp>
2018-07-26 13:45:49 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -45,6 +45,13 @@
         twttr.widgets.load();
     };
 
+    useNameInput.onkeydown=(event) => {
+        if(event.keyCode == 14){
+            assesmentButton.onclick();
+            }
+        };
+    
+    
     const answers = [
         '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
         '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
